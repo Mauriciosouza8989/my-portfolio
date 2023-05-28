764085a30ada0cd42b3136b6647485d2c9a622cd
Mauricio <mauriciosouza8989@gmail.com>
2023-05-28 16:56:07 -0300

    updated directory

--- src/components/stacks/index.jsx
+++ src/components/stacks/index.jsx
@@ -7,56 +7,56 @@ export function Stacks(){
         <div className="cards">
             <CardStack
               id="html"
-              src="../src/assets/html.png"
+              src="../../assets/html.png"
               alt="imagem html"
               title="HTML"
             />
 
             <CardStack
               id="css"
-              src="../src/assets/css3-logo.png"
+              src="../../assets/css3-logo.png"
               alt="imagem css"
               title="CSS"
             />
             
             <CardStack
               id="js"
-              src="../src/assets/js1.png"
+              src="../../assets/js1.png"
               alt="imagem javascript"
               title="JAVASCRIPT"
             />
           
           <CardStack
               id="github"
-              src="../src/assets/github-mark/github-mark.svg"
+              src="../../assets/github-mark/github-mark.svg"
               alt="imagem github"
               title="GITHUB"
             />
             
             <CardStack
               id="linux"
-              src="../src/assets/linux.png"
+              src="../../assets/linux.png"
               alt="imagem linux"
               title="LINUX"
             />
             
             <CardStack
               id="node"
-              src="../src/assets/node.png"
+              src="../../assets/node.png"
               alt="imagem do nodejs"
               title="NODE JS"
             />
 
             <CardStack
               id="react"
-              src="../src/assets/react.png"
+              src="../../assets/react.png"
               alt="imagem da lib react"
               title="REACT JS"
             />
             
             <CardStack
               id="sql"
-              src="../src/assets/sqlite.png"
+              src="../../assets/sqlite.png"
               alt="imagem de banco de dados sql"
               title="SQLITE"
             />
