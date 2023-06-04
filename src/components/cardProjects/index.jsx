22697b606ea528a146d6d089cc4343c2d4f0b99d
Mauricio <mauriciosouza8989@gmail.com>
2023-06-04 13:41:27 -0300

    added target='blank'

--- src/components/cardProjects/index.jsx
+++ src/components/cardProjects/index.jsx
@@ -4,12 +4,12 @@
 export function CardProject({deploy, github, src, alt, title }){
     return(
         <div className="card">
-        <a href={deploy} >
+        <a href={deploy} target="blank">
             <img src={src} alt={alt}/>
         </a>
         <p>{title}</p>
-        <a href={deploy}>Ver site</a>
-        <a href={github} >Ver no github</a>
+        <a href={deploy} target="blank">Ver site</a>
+        <a href={github} target="blank">Ver no github</a>
     </div>
     )
 }
